c247828a3934bb1213f80bccc614035d903fe96e
StudentForEternity <109116999+StudentForEternity@users.noreply.github.com>
2023-03-18 16:27:16 -0400

    APIs/Errors/Navigation improvement

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createServer, Model } from "miragejs"
+import { createServer, Model, Response } from "miragejs"
 
 
 createServer({
@@ -18,8 +18,10 @@ createServer({
     routes() {
         this.namespace = "api"
         this.logging = false
+        this.timing = 2000
 
         this.get("/vans", (schema, request) => {
+            // return new Response(400, {}, {error: "Error fetching data"})
             return schema.vans.all()
         })
 
@@ -29,12 +31,12 @@ createServer({
         })
 
         this.get("/host/vans", (schema, request) => {
-            // Hard-code the hostId for now... for easier project building
+            // Hard-code the hostId for now
             return schema.vans.where({ hostId: "123" })
         })
 
         this.get("/host/vans/:id", (schema, request) => {
-            // Hard-code the hostId for now... for easier project building
+            // Hard-code the hostId for now
             const id = request.params.id
             return schema.vans.findBy({ id, hostId: "123" })
         })
